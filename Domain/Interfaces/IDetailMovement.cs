604b36cd090bef8099781314e92d5057a57aef66
Giovanny Moreno <giovannysocas@gmail.com>
2023-10-19 01:25:13 -0500

    feat: Creacion de interfaces

---
+++ Domain/Interfaces/IDetailMovement.cs
@@ -0,0 +1,11 @@
+
+
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IDetailMovement : IGenericRepository<DetailMovement>
+    {
+        
+    }
+}
---
+++ Domain/Interfaces/IMedicine.cs
@@ -0,0 +1,10 @@
+
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IMedicine : IGenericRepository<Medicine>
+    {
+        
+    }
+}
