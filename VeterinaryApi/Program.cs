cf65bd8b3cce6c802542893c71bb3fdf50e2b756
Giovanny Moreno <giovannysocas@gmail.com>
2023-10-19 01:00:28 -0500

    feat: Skeleto Controller, extensions y program

--- VeterinaryApi/Program.cs
+++ VeterinaryApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using VeterinaryApi.Extension;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureCors();
 
 builder.Services.AddDbContext<VeterinaryContext>(options =>
 {
@@ -24,7 +26,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}   
+app.UseCors("CorsPolicy");  
 
 app.UseHttpsRedirection();
 
